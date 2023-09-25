8bf3642f83b51e61a5959aa4e5c66f37f4baf7f1
Dizzydoze <overdosedizzy@gmail.com>
2023-09-24 20:18:47 -0700

    modified 58 solution

--- src/58-LengthofLastWord.py
+++ src/58-LengthofLastWord.py
@@ -1,23 +1,16 @@
 class Solution:
     """
-    lost of ways to do it, two pointers, list comprehension, etc.
+    lost of ways to do it, loop backward, list comprehension, etc.
     """
     def lengthOfLastWord(self, s: str) -> int:
-        # two pointers up and down
-        u = d = len(s) - 1
-        while True:
-            # last word found as reaching non-empty spot
-            if s[d] != " ":
-                # u start to measure length
-                u = d
-                while u >= 0:
-                    if s[u] != " ":
-                        u -= 1
-                    # last word ends as we reach empty again
-                    else:
-                        return d - u
-                # u < 0, there's only one word in the list
-                return d - u
-            # moving d backward to find the last word
-            else:
-                d -= 1
+        res, last = 0, False
+        # loop from the back
+        for i in range(len(s) - 1, -1, -1):
+            # s[i] first letter of last word
+            if s[i] != " ":
+                last = True
+                while i >= 0 and s[i] != " ":
+                    res += 1
+                    i -= 1
+            if last:
+                return res
