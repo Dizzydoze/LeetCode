9b84e9ec8bec9a413ede130edfae34e0432494c1
Dizzydoze <overdosedizzy@gmail.com>
2023-10-12 17:36:01 -0700

    389 one more solution

--- src/389-FindtheDifference.py
+++ src/389-FindtheDifference.py
@@ -7,3 +7,15 @@ class Solution:
         for c in t:
             tnum += ord(c)
         return chr(tnum - snum)
+
+
+class Solution2:
+    """sorting"""
+    def findTheDifference(self, s: str, t: str) -> str:
+        s = sorted([c for c in s])
+        t = sorted([c for c in t])
+        for i in range(len(s)):
+            if s[i] != t[i]:
+                return t[i]
+        # or the last char is different
+        return t[-1]
