27fdec3c4a849b30e40274f890f9b4a994c4f922
Dizzydoze <overdosedizzy@gmail.com>
2023-11-09 23:06:03 -0800

    Restructure the whole project into different directories.

--- zsrc/231-PowerofTwo.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def isPowerOfTwo(self, n: int) -> bool:
-        # math
-        for i in range(31):
-            if 2 ** i == n:
-                return True
-        return False
-
-# best practice bit manipulation, to be continue...
